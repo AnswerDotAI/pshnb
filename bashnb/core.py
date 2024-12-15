9cf5618ad84f40c4a91855203b4614ab91f6561c
Jeremy Howard <github@jhoward.fastmail.fm>
2024-12-15 19:41:37 +1000

    basic working nb

--- bashnb/core.py
+++ bashnb/core.py
@@ -6,33 +6,30 @@
 __all__ = ['BashMagic', 'create_magic', 'load_ipython_extension', 'create_ipython_config']
 
 # %% ../00_core.ipynb
-import re,os
+import pexpect, re, os
 from pathlib import Path
 from IPython.core.magic import register_cell_magic
 from IPython.display import display, Javascript
 from IPython.paths import get_ipython_dir
+from IPython.core.interactiveshell import InteractiveShell
 
 # %% ../00_core.ipynb
 class BashMagic:
     def __init__(self):
-        self.o = ...
-        self._loaded = False
+        self.o = ShellInterpreter()
 
     def bash(self, line, cell=None):
         if line and not cell: cell=line
-        if not self._loaded:
-            p = Path(__file__).resolve().parent
-            self._loaded = True
         disp = True
         if cell.endswith(';'): disp,cell = False,cell[:-1]
-        res = self.o.eval(cell) or None
-        if disp: return res
+        res = self.o(cell) or None
+        if disp: print(res)
 
 # %% ../00_core.ipynb
 def create_magic(shell=None):
     if not shell: shell = get_ipython()
     bash_magic = BashMagic()
-    shell.register_magic_function(bash_magic, 'line_cell', 'psh')
+    shell.register_magic_function(bash_magic.bash, 'line_cell', 'psh')
 
 # %% ../00_core.ipynb
 def load_ipython_extension(ipython):
